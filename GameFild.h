2f421fd9c2f39a22ca3048e109831070c44ee7eb
Oleg <olegshutko54@gmail.com>
2023-10-20 17:30:09 +0300

    Git init

---
+++ GameFild.h
@@ -0,0 +1,5 @@
+#ifndef Fild
+#define Fild
+
+
+#endif
---
+++ Main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+#include "Cell.h"
+#include "GameFild.h"
+
+using namespace std;
+
+int main(){
+    
+    return 0;
+}
